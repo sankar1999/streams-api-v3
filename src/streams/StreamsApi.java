09f30d252ae73c9e67add8d7a6ecec7a4231c4c8
sankar1999 <sankarsathya1973@gmail.com>
2022-11-11 02:43:31 +0530

    Added something

--- src/streams/StreamsApi.java
+++ src/streams/StreamsApi.java
@@ -2,21 +2,30 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class StreamsApi {
 
     public static void main(String[] args) {
 
+        System.out.println(atomicBreak());
+    }
+
+    public static boolean atomicBreak() {
+
         List<String> countries = Arrays.asList("India", "America", "Dubai");
+
+        AtomicBoolean breakFound = new AtomicBoolean(false);
 
         AtomicInteger countryMatchCount = new AtomicInteger();
 
         countries.stream()
-                .filter(country -> country.endsWith("a"))
+                .filter(country -> country.endsWith("i"))
                 .forEach(country -> countryMatchCount.getAndIncrement());
+        System.out.println(countryMatchCount.incrementAndGet());
+        return !breakFound.get();
 
-        System.out.println(countryMatchCount.get());
     }
 
 
